d93946f40bb4f4cf9df42af95d32c9aecd6cbd92
Rixchiolina <recchiafabio04@gmail.com>
2025-04-14 15:39:35 +0200

    change for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.argv.includes('dev');
-
 export default {
-	preprocess: preprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'app.html' 
-		}),
-		paths: {
-			base: dev ? '' : '/womblab-site'
-		}
-	}
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
